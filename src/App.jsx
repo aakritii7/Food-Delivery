794e5eb84347bb2b9efd421391b133ec72da1685
Kidddooo <mahatoaakriti3@gmail.com>
2026-08-10 00:06:31 +0545

    react-router-dom install

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   )
 }
