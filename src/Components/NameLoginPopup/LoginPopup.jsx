7212c83fc0281ecd11fabaf61099137021ea16eb
Kidddooo <mahatoaakriti3@gmail.com>
2026-08-18 23:49:54 +0545

    Login Statemanagement is still working

--- src/Components/NameLoginPopup/LoginPopup.jsx
+++ src/Components/NameLoginPopup/LoginPopup.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './LoginPopup.css'
+import { assets } from '../../assets/assets'
 
-const LoginPopup = () => {
+const LoginPopup = ({setShowLogin}) => {
+    const [currState,setCurrState] = useState("Login")
   return (
     <div className='login-popup'>
-        
+        <form className="login-popup-container">
+            <div className="login-popup-title">
+                <h2>{currState}</h2>
+                <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+            </div>
+            <div className="login-popup-inputs">
+                {currState==='Login'?<></>:<input type="text" placeholder='Your name' required />}
+                
+                <input type="email" placeholder='Your email' required />
+                <input type="password" placeholder='Password' required />
+            </div>
+            <button>{currState==="Sign Up"? "Create account" : "Login"}</button>
+        </form>
     </div>
   )
 }
